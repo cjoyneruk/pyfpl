117905936d4cf9e54d6260020897c0a101964354
= <c.joyner@qmul.ac.uk>
2019-09-04 18:40:45 +0100

    minor change to past_season_test.py

--- tests/past_season_test.py
+++ tests/past_season_test.py
@@ -6,7 +6,7 @@ cols = ['element', 'clubid', 'matchid', 'GW', 'was_sub',
        'status', 'position', 'chance_of_playing_this_round', 'chance_of_playing_next_round']
 
 
-sd = datasets.pastSeason(season=2018, cols=cols)
+sd = datasets.seasonData(season=2018, cols=cols)
 
 df = sd[sd['status']=='i']
 
