e6a80dc89fe9b246d14c3a37d8e4bdd28917a046
= <c.joyner@qmul.ac.uk>
2019-09-04 19:06:30 +0100

    minor change to past_season_test.py

--- tests/past_season_test.py
+++ tests/past_season_test.py
@@ -2,11 +2,33 @@ from pyfpl import datasets
 import numpy as np
 
 cols = ['element', 'clubid', 'matchid', 'GW', 'was_sub',
-       'in_squad', 'minutes',  'web_name', 'value',
+       'in_squad', 'minutes',  'web_name', 'value', 'match_no',
        'status', 'position', 'chance_of_playing_this_round', 'chance_of_playing_next_round']
-
 
 sd = datasets.seasonData(season=2018, cols=cols)
 
-df = sd[sd['status']=='i']
+# - Construct baseline: If started previous game and available then predict start, otherwise not
 
+# - Create started column
+sd['start'] = (True^sd['was_sub'])&sd['in_squad']
+
+# - Create started previous game column
+alt_df = sd[['element', 'match_no', 'start']]
+alt_df = alt_df.rename(columns={'start': 'start_prev'})
+alt_df['match_no'] += 1
+
+sd = sd.merge(alt_df, on=['element', 'match_no'], how='left')
+
+sd = sd.drop(sd[sd['match_no']==1].index)
+sd = sd.reset_index(drop=True)
+
+X = sd[['start_prev', 'status']]
+y = sd['start']
+
+y_pred = X['start_prev']&(X['status']=='a')
+
+n = y.shape[0]
+n_c = sum(y==y_pred)
+n_i = n - n_c
+
+print(n_c/n)
